ece7b789ed9fee7231deb85d3c02d63c5dc4ccf0
ankitgirdhar <stanleycooper136@gmail.com>
2020-06-29 00:01:13 +0530

    react

---
+++ src/main/resources/static/precache-manifest.053d92aa3425a79f94cb7d87c3e81234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66bbf9ef349f18285d2690173d4ab3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9ad273ca64b8b10e93",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "87fa37e241d7e9e62dc9",
+    "url": "/static/css/main.e79392dd.chunk.css"
+  },
+  {
+    "revision": "0b9ad273ca64b8b10e93",
+    "url": "/static/js/2.e1b056b0.chunk.js"
+  },
+  {
+    "revision": "780dcd34534fbf6955ad8d91034e2a51",
+    "url": "/static/js/2.e1b056b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87fa37e241d7e9e62dc9",
+    "url": "/static/js/main.9e2a6468.chunk.js"
+  },
+  {
+    "revision": "e63a5d9d7e086e83e2d3",
+    "url": "/static/js/runtime-main.9dc8b6e7.js"
+  }
+]);
